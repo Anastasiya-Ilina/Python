18621a708e52c59e038d40e8faee714118eb2554
Anastasiya-Ilina <119570132+Anastasiya-Ilina@users.noreply.github.com>
2022-12-13 12:15:30 +0300

    Create dz4_2.py

---
+++ LessPy4/dz4_2.py
@@ -0,0 +1,3 @@
+key = ['Salmon', 'HotPink', 'Coral', 'Moccasin', 'Indigo', 'Chocolate']
+rgb = [(250, 128, 114), (255, 105, 180), (255, 127, 80), (255, 228, 181), (75, 0, 130), (210, 105, 30)]
+print(dict(zip(key, rgb)))

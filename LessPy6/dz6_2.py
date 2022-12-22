e5c37b532162c9601e46a71107686d5bbab22ac7
Anastasiya-Ilina <119570132+Anastasiya-Ilina@users.noreply.github.com>
2022-12-22 14:12:58 +0300

    Update dz6_2.py

--- LessPy6/dz6_2.py
+++ LessPy6/dz6_2.py
@@ -1,9 +1,11 @@
 import io
 import math
 with io.open('Input.txt', encoding='utf-8') as f:
-        a = f.read().split()
+        a = f.read().split()    # Считывает значения в файле
 print('Начальные значения:', a)
-C = math.floor(int(a[0])/2)
-H = math.floor(int(a[1])/6)
-O = math.floor(int(a[2])/1)
+
+C = math.floor(int(a[0])/2)    # Определяет целое число атомов углерода
+H = math.floor(int(a[1])/6)    # Определяет целое число атомов водорода
+O = math.floor(int(a[2])/1)    # Определяет целое число атомов кислорода
+
 print('Максимально возможно число молекул спирта: ', min(C, H, O))
